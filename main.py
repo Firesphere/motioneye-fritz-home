6e25a6cd0afb928c9aa51915b0539b84e1737d87
Simon `Firesphere` <firesphere@casa-laguna.net>
2022-08-23 21:28:28 +1200

    Add some features

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ logger = logging.getLogger("Reboot logger")
 logger.setLevel(logging.INFO)
 if os.getenv('ENVIRONMENT', 'prod') == 'dev':
     handler = logging.StreamHandler(sys.stdout)
-    node_name = 'prelude' # My debugging name
+    node_name = 'prelude'  # My debugging name
 else:  # The else is to prevent errors trying to open the log file as the wrong user
     handler = logging.FileHandler('/var/log/mqtt_reboot.log')
     node_name = os.uname().nodename.lower()
@@ -27,9 +27,15 @@ mqtt_host = os.getenv('MQTT_HOST', '127.0.0.1')
 mqtt_port = os.getenv('MQTT_PORT', 1883)
 mqtt_user = os.getenv('MQTT_USER', "")
 mqtt_pass = os.getenv('MQTT_PASS', "")
-report_topic = os.getenv('MQTT_TOPIC').format(node_name) + 'available'
-command_topic = os.getenv('MQTT_TOPIC').format(node_name) + 'cmd'
+mqtt_topc = os.getenv('MQTT_TOPC', "")
+report_topic = '{}/{}/{}'.format(mqtt_topc, node_name, 'available')
+report_me_topic = '{}/{}/{}'.format(mqtt_topc, node_name, 'motioneye')
+command_topic = '{}/{}/{}'.format(mqtt_topc, node_name, 'cmd')
 mqtt_client = mqtt.Client()
+mqtt_client.username_pw_set(mqtt_user, mqtt_pass)
+mqtt_client.will_set(report_topic, "offline", 1, True)
+mqtt_client.will_set(report_me_topic, "offline", 1, True)
+mqtt_client.connect(mqtt_host, mqtt_port)  # Connect to the test MQTT broker
 
 
 # "on connect" event
@@ -57,15 +63,14 @@ def handle_command(cmd):
         call(['reboot'], shell=False)  # reboot host
     elif cmd == "shutdown":
         call(['shutdown', '-h', 'now'], shell=False)  # shut down host
+    elif cmd == "motioneye":
+        call("service motioneye start", shell=True)  # Start motioneye
+        mqtt_client.publish(report_me_topic, "online", 1, True)
     elif cmd == "test":
         mqtt_client.publish(report_topic, "Reply to test msg")  # Publish reply to an incoming msg with payload "test"
 
 
 if __name__ == '__main__':
-    mqtt_client.username_pw_set(mqtt_user, mqtt_pass)
-    while True:
-        mqtt_client.on_connect = connect  # run function on connect with broker
-        mqtt_client.will_set(report_topic, "offline", 1, True)
-        mqtt_client.connect(mqtt_host, mqtt_port)  # Connect to the test MQTT broker
-        mqtt_client.on_message = read_message  # Attach the messageFunction to subscription
-        mqtt_client.loop_forever()
+    mqtt_client.on_connect = connect  # run function on connect with broker
+    mqtt_client.on_message = read_message  # Attach the messageFunction to subscription
+    mqtt_client.loop_forever()
